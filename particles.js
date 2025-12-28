5877f412c3a76691ffdaf25e7d3c4711178b5b51
jaybarve <jay52907420@gmail.com>
2025-12-28 15:55:33 +0530

    Create particles.js

---
+++ particles.js
@@ -0,0 +1,29 @@
+const canvas=document.getElementById("particles");
+const ctx=canvas.getContext("2d");
+canvas.width=innerWidth; canvas.height=innerHeight;
+
+let particles=[];
+for(let i=0;i<80;i++){
+ particles.push({
+  x:Math.random()*canvas.width,
+  y:Math.random()*canvas.height,
+  r:Math.random()*2+1,
+  dx:(Math.random()-.5),
+  dy:(Math.random()-.5)
+ });
+}
+
+function animate(){
+ ctx.clearRect(0,0,canvas.width,canvas.height);
+ particles.forEach(p=>{
+  p.x+=p.dx; p.y+=p.dy;
+  if(p.x<0||p.x>canvas.width)p.dx*=-1;
+  if(p.y<0||p.y>canvas.height)p.dy*=-1;
+  ctx.fillStyle="#00cfff";
+  ctx.beginPath();
+  ctx.arc(p.x,p.y,p.r,0,Math.PI*2);
+  ctx.fill();
+ });
+ requestAnimationFrame(animate);
+}
+animate();
